6ae33a96bcf53def22818bfd8f879a53dd260a12
jamshedd89 <jamshedd_89@mail.ru>
2025-06-19 16:37:29 +0500

    edit ios file

--- ios/Sources/TootiPlugin/TootiPlugin.swift
+++ ios/Sources/TootiPlugin/TootiPlugin.swift
@@ -1,12 +1,129 @@
 import Foundation
 import Capacitor
+import AVFoundation
+import UIKit
+
+@objc(TootiPlugin)
+public class TootiPlugin: CAPPlugin {
+    private var call: CAPPluginCall?
+    private var qrViewController: QRViewController?
+
+    @objc func scanQR(_ call: CAPPluginCall) {
+        self.call = call
+        DispatchQueue.main.async {
+            self.qrViewController = QRViewController()
+            self.qrViewController?.delegate = self
+            self.bridge?.viewController?.present(self.qrViewController!, animated: true)
+        }
+    }
+}
+
+// MARK: - QRViewControllerDelegate
+
+extension TootiPlugin: QRViewControllerDelegate {
+    func didScanQRCode(value: String) {
+        call?.resolve([
+            "value": value
+        ])
+        qrViewController?.dismiss(animated: true)
+        qrViewController = nil
+    }
+
+    func didCancelQRCode() {
+        call?.reject("cancelled")
+        qrViewController?.dismiss(animated: true)
+        qrViewController = nil
+    }
+}
+
+// MARK: - QRViewController
+
+protocol QRViewControllerDelegate: AnyObject {
+    func didScanQRCode(value: String)
+    func didCancelQRCode()
+}
+
+class QRViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    weak var delegate: QRViewControllerDelegate?
+    var captureSession: AVCaptureSession?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        setupCaptureSession()
+
+        // Кнопка "Закрыть"
+        let closeButton = UIButton(type: .system)
+        closeButton.setTitle("Закрыть", for: .normal)
+        closeButton.addTarget(self, action: #selector(didTapClose), for: .touchUpInside)
+        closeButton.frame = CGRect(x: 20, y: 40, width: 100, height: 40)
+        view.addSubview(closeButton)
+    }
+
+    func setupCaptureSession() {
+        captureSession = AVCaptureSession()
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let videoInput = try? AVCaptureDeviceInput(device: videoCaptureDevice) else { return }
+        if (captureSession?.canAddInput(videoInput) == true) {
+            captureSession?.addInput(videoInput)
+        } else {
+            delegate?.didCancelQRCode()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+        if (captureSession?.canAddOutput(metadataOutput) == true) {
+            captureSession?.addOutput(metadataOutput)
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            delegate?.didCancelQRCode()
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+        previewLayer?.frame = view.layer.bounds
+        previewLayer?.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer!)
+        captureSession?.startRunning()
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession?.stopRunning()
+        if let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
+           let stringValue = metadataObject.stringValue {
+            delegate?.didScanQRCode(value: stringValue)
+        } else {
+            delegate?.didCancelQRCode()
+        }
+    }
+
+    @objc func didTapClose() {
+        captureSession?.stopRunning()
+        delegate?.didCancelQRCode()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        captureSession?.stopRunning()
+    }
+}
+
+
+
+/**
+import Foundation
+import Capacitor
 import UIKit
 import SwiftUI
 import SwiftQRScanner
+*/
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
+/**
 @objc(TootiPlugin)
 public class TootiPlugin: CAPPlugin, CAPBridgedPlugin,QRScannerCodeDelegate {
     public let identifier = "TootiPlugin"
@@ -60,5 +177,5 @@ public class TootiPlugin: CAPPlugin, CAPBridgedPlugin,QRScannerCodeDelegate {
     
 }
 
-
+*/
 
